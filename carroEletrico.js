dde6331681f14e33d394a05dde6ea70c1aa7bb1c
Emanuelle silva <campos.silva.emanuelle@escola.pr.gov.br>
2026-07-28 12:11:27 -0300

    Create carroEletrico.js

---
+++ carroEletrico.js
@@ -0,0 +1,37 @@
+class carroEletrico extends Carro {
+    constructor(marca, modelo, ano, capacidadeBateria) {
+        super(marca, modelo, ano);
+        this.capacidadeBateria = capacidadeBateria;
+
+    }
+    imprimirDetalhes() {
+        return
+        `
+        Marca:${this.marca}
+        Modelo:${this.modelo}
+        Ano:${this.ano}
+        capacidade da Bateria:${this.capacidadeBateria} KWh
+        `
+
+    }
+}
+
+const carro1 = new Carro(
+    "Toyota",
+    "Corolla",
+    2022
+);
+
+const carro2 = new carroEletrico(
+    "Tesla",
+    "Model 3",
+    2024,
+    75
+);
+function mostrarCarros() {
+    document.getElementById("resultado").innerHTML =
+        "<b>Carro Convencional</b><br>" +
+        carro1.imprimirDetalhes() +
+        "<br><br><b>Carro Eletrico</b><br>" +
+        carro2.imprimirDetalhes();
+}
